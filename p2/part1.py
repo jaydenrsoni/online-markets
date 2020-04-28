7af6ed0c78a0cd1dd5d22e7f407af98d8e5e6ca3
Jayden Soni <jrs@u.northwestern.edu>
2020-04-28 15:27:20 -0700

    ftpl

--- p2/part1.py
+++ p2/part1.py
@@ -20,6 +20,7 @@ def exponential_weights(data, lr, h):
     action_payoffs = [0] * len(data[0])
     actions = []
     total_payoff = 0
+
     for round in data:
         probabilities = []
         sum_probabilities = 0
@@ -29,7 +30,7 @@ def exponential_weights(data, lr, h):
             sum_probabilities += p
         probabilities = [p/sum_probabilities for p in probabilities]
         print(probabilities)
-        
+
         action = np.random.choice(len(round), 1, p=probabilities)[0]
         actions.append(action)
         total_payoff += round[action]
@@ -45,10 +46,34 @@ def exponential_weights(data, lr, h):
     print(regret)
 
 
-def follow_the_perturbed_leader(data, lr,h):
-    k = 10 # num actions
+def follow_the_perturbed_leader(data, lr, h):
+    k = len(data[0])  # num actions
     hallucinations = np.random.geometric(p=lr, size=k) * h
     print(hallucinations)
+
+    action_payoffs = [0] * len(data[0])
+    actions = []
+    total_payoff = 0
+
+    for round in data:
+        action = np.argmax(np.add(hallucinations, action_payoffs))
+        actions.append(action)
+        total_payoff += round[action]
+
+        # update V
+        for i in range(len(round)):
+            action_payoffs[i] += round[i]
+        print("action payoffs: ", action_payoffs)
+    print(total_payoff, actions)
+
+    best_in_hindsight_payoff = max(action_payoffs)
+    regret = (best_in_hindsight_payoff - total_payoff) / len(data)
+    print(regret)
+
+
+def online_learning(data, lr, h, type="ew"):
+
+
 
 if __name__ == '__main__':
     data1 = dataset1(10, 3, [0.5] * 3)
