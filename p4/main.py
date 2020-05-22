e6873c29a5a8298003a0fdda8ad0c714b200bd10
Jayden Soni <jrs@u.northwestern.edu>
2020-05-22 15:49:34 -0700

    added proj2 alg

--- p4/main.py
+++ p4/main.py
@@ -6,5 +6,51 @@ def online_reserve_pricing():
     return None
 
 
+# algorithm from project 2 for ew/ftpl
+def online_learning(data, lr, h, algo="ew", verbose=True):
+    k = len(data[0])  # num actions
+    hallucinations = np.random.geometric(p=lr, size=k) * h  # only used for ftpl
+    if algo == "ftpl":
+        print("hallucinations: ", hallucinations)
+
+    action_payoffs = [0] * k  # V vector holding cumulative payoffs of each action
+    actions = []  # list of actions algorithm takes
+    total_payoff = 0  # payoff of actions algorithm takes
+
+    # loop through payoffs round-by-round
+    for curr_payoffs in data:
+
+        # choose an action action using given algorithm
+        if algo == "ew":
+            raw_probabilities = [math.pow((1 + lr), action_payoffs[j]/h) for j in range(k)]
+            norm_probabilities = np.divide(raw_probabilities, math.fsum(raw_probabilities))
+            action = np.random.choice(k, 1, p=norm_probabilities)[0]
+            if verbose:
+                print("probabilities: ", norm_probabilities)
+
+        elif algo == "ftpl":
+            action = np.argmax(np.add(hallucinations, action_payoffs))
+
+        else:
+            raise Exception("not a valid algorithm")
+
+        actions.append(action)
+        total_payoff += curr_payoffs[action]
+
+        # update V with payoffs from current round
+        action_payoffs = np.add(action_payoffs, curr_payoffs)
+        if verbose:
+            print("action payoffs: ", action_payoffs)
+
+    # calculate OPT and regret
+    best_in_hindsight_payoff = max(action_payoffs)
+    regret = (best_in_hindsight_payoff - total_payoff) / len(data)
+
+    print("actions (max of last 25 shown): ", actions[len(actions) - 20:])
+    print("total payoff: ", total_payoff)
+    print("regret: ", regret)
+    return total_payoff, best_in_hindsight_payoff
+
+
 if __name__ == '__main__':
     print("Part 1")
